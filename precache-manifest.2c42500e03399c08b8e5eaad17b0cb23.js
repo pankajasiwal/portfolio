ccef0543bd1004aef76ea890775d89d1789a24dd
pankajasiwal <pankajasiwal1234@gmail.com>
2023-07-21 14:19:25 +0530

    Updates

---
+++ precache-manifest.2c42500e03399c08b8e5eaad17b0cb23.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "d41f4820fc4381ef30a9",
+    "url": "/portfolio/static/css/main.d9f183df.chunk.css"
+  },
+  {
+    "revision": "d41f4820fc4381ef30a9",
+    "url": "/portfolio/static/js/main.d41f4820.chunk.js"
+  },
+  {
+    "revision": "5bff56686b744a6e6287",
+    "url": "/portfolio/static/js/1.5bff5668.chunk.js"
+  },
+  {
+    "revision": "56aff611cf4690432043",
+    "url": "/portfolio/static/js/2.56aff611.chunk.js"
+  },
+  {
+    "revision": "bd7f15f4bde2e457d842",
+    "url": "/portfolio/static/js/runtime~main.bd7f15f4.js"
+  },
+  {
+    "revision": "28a13e7ba67f62db1fe46fd4477582da",
+    "url": "/portfolio/static/media/web-development.28a13e7b.jpg"
+  },
+  {
+    "revision": "779ff3093837eb278885feac066443a1",
+    "url": "/portfolio/static/media/python.779ff309.jpg"
+  },
+  {
+    "revision": "5ee3d70de48bddcb79d00fe9e8710389",
+    "url": "/portfolio/static/media/iot.5ee3d70d.jpg"
+  },
+  {
+    "revision": "189edaf590d9ff5941a0247aac2a6f21",
+    "url": "/portfolio/static/media/backgroundImage-pankaj.189edaf5.jpg"
+  },
+  {
+    "revision": "01401a0f4cb711f2fe4d9857a9da7641",
+    "url": "/portfolio/static/media/profile-image-pankaj.01401a0f.jpg"
+  },
+  {
+    "revision": "651f3de8bc3f9c6b957d413908f67993",
+    "url": "/portfolio/index.html"
+  }
+];
